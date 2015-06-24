7d120159cf1dbac6f4e52c126d11b654dcc7f8c6
Oleg Sesov <oleg.sesov@dev.zodiac.tv>
2015-06-24 13:40:11 -0400

    more options to zupload

--- emacs/init.el
+++ emacs/init.el
@@ -14,3 +14,21 @@
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode t)
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+;; markdown
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; zmk
+(load-theme 'manoj-dark)
+
+(global-set-key (kbd "<f9>") 'compile)
+(global-set-key (kbd "<f4>") 'next-error)
+(global-set-key (kbd "S-<f4>") 'previous-error)
+
+(setenv "ENABLE_DIRTRACE" "1")
+(setenv "DISABLE_COLORS" "1")
+(setq compile-command "zmk")
